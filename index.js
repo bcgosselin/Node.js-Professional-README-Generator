f0065b01a1a05f0f3905ec20a2a38e3452d3504e
bcgosselin <bcgosselin@gmail.com>
2024-02-29 21:21:01 -0600

    Filled out functions missing array in index.js

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
-// TODO: Include packages needed for this application
+// packages needed
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// questions array
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
-// TODO: Create a function to write README file
+// function that writes README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
+    fs.writeFile(fileName, data, error => {
         if (error) {
             console.error(error);
             return;
         }
 
-        console.log("README.md successfully generated!");
+        console.log("README.md added to directory");
     });
 }
 
-// TODO: Create a function to initialize app
+// function that initializes app
 function init() {
     inquirer.prompt(questions)
         .then(answers => {
@@ -30,5 +77,5 @@ function init() {
         });
 }
 
-// Function call to initialize app
+// Function call that initializes app
 init();
